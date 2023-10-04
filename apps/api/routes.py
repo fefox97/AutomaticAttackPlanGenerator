fa063d0cd5b117752da619340a7a4e5caa1e4d28
Felice Moretta <felice_moretta@hotmail.it>
2023-10-04 02:55:37 +0200

    Modificata posizione del metodo per i file ammessi Aggiunta api per il caricamento dei dataframe nei database

--- apps/api/routes.py
+++ apps/api/routes.py
@@ -9,10 +9,8 @@ from flask_login import login_required, current_user
 from jinja2 import TemplateNotFound
 from flask import current_app as app
 from flask import jsonify
-from apps.my_modules import converter, attack_pattern, threat_catalog, macm
-from werkzeug.utils import secure_filename
-from apps import utils
-from . import attack_pattern_utils
+from apps.my_modules import converter, macm, utils
+from apps.api.utils import AttackPatternAPIUtils, APIUtils
 
 # @login_required
 @blueprint.route('/<api>', methods=['GET', 'POST'])
@@ -27,15 +25,14 @@ def route_api(api):
             if api == 'search_capec_by_id':
                 search_id = request.form.get("SearchID") or ''
                 showTree = True if request.form.get("ShowTree") == 'true' else False
-                getDataframe = True if request.form.get("GetDataframe") == 'true' else False
                 search_id_conv = converter.string_to_int_list(search_id)
-                childs = attack_pattern_utils.get_child_attack_patterns(search_id_conv, show_tree=showTree)
+                childs = AttackPatternAPIUtils().get_child_attack_patterns(search_id_conv, show_tree=showTree)
                 return jsonify({'childs': childs})
 
             elif api == 'upload_macm':
                 if 'macmFile' in request.files:
                     file = request.files['macmFile']
-                    if not utils.allowed_file(file.filename, 'txt'):
+                    if not APIUtils.allowed_file(file.filename, 'txt', app.config['ALLOWED_EXTENSIONS']):
                         return jsonify({'success': False, 'message': 'File type not allowed'})
                     query_str = file.read().decode('utf-8')
                 elif 'macmCypher' in request.form:
@@ -45,6 +42,14 @@ def route_api(api):
                 macm.upload_macm(query_str)
                 # macm.create_enhanced_macm()
                 return redirect(url_for('home_blueprint.route_template', template='macm.html'))
+            
+            elif api == 'reload_databases':
+                database = request.form.get('database')
+                if database == 'Capec':
+                    utils.upload_databases('Capec')
+                elif database == 'ThreatCatalog':
+                    utils.upload_databases('ThreatCatalog')
+                return jsonify({'success': True, 'message': f'Database {database} reloaded'})
         
         elif request.method == 'GET':
             if api == 'clear_macm_database':
