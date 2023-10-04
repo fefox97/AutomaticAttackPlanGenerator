2ce17cac9a7d1f6b4e1701f85f19295ba81404bc
Felice Moretta <felice_moretta@hotmail.it>
2023-10-04 02:59:08 +0200

    Aggiunto modello del database del MACM e del database di associazioni tra Capec e Threat Catalog

--- apps/databases/models.py
+++ apps/databases/models.py
@@ -4,9 +4,11 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 
-from sqlalchemy.orm import relationship
+from typing import List
+from sqlalchemy.orm import relationship, Mapped
 from sqlalchemy.sql.expression import case
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy import ForeignKey
 
 from apps import db
 
@@ -14,7 +16,7 @@ class Capec(db.Model):
 
     __tablename__ = 'Capec'
 
-    Capec_ID                = db.Column(db.Integer, primary_key=True)
+    Capec_ID                = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     Created                 = db.Column(db.TIMESTAMP)
     Created_By_Ref          = db.Column(db.JSON)
     Description             = db.Column(db.Text)
@@ -68,7 +70,7 @@ class ThreatCatalog(db.Model):
 
     __tablename__ = 'ThreatCatalog'
 
-    TID                 = db.Column(db.Text, primary_key=True)
+    TID                 = db.Column(db.Text, primary_key=True, unique=True, nullable=False)
     Asset               = db.Column(db.Text)
     Threat              = db.Column(db.Text)
     Description         = db.Column(db.Text)
@@ -86,6 +88,9 @@ class ThreatCatalog(db.Model):
     CapecStandard       = db.Column(db.JSON)
     CapecDetailed       = db.Column(db.JSON)
     Commento            = db.Column(db.Text)
+    
+    CapecMetaRel: Mapped[List[Capec]] = relationship("Capec", secondary="AssociationTable", backref="ThreatCatalog")
+
 
     def __init__(self, **kwargs):
         for property, value in kwargs.items():
@@ -100,3 +105,35 @@ class ThreatCatalog(db.Model):
 
     def __repr__(self):
         return str(self.Asset)
+    
+class Association(db.Model):
+
+    __tablename__ = 'AssociationTable'
+
+    Capec_ID                  = db.Column(db.Integer, ForeignKey("Capec.Capec_ID"), primary_key=True, unique=True, nullable=False)
+    ThreatCatalog_ID          = db.Column(db.Text, ForeignKey("ThreatCatalog.TID"), primary_key=True, unique=True, nullable=False)
+class Macm(db.Model):
+
+    __tablename__ = 'Macm'
+
+    Component_ID    = db.Column(db.Integer, primary_key=True)
+    Application     = db.Column(db.Text)
+    Name            = db.Column(db.Text)
+    Type            = db.Column(db.Text)
+    App_ID          = db.Column(db.Integer)
+    
+
+    def __init__(self, **kwargs):
+        for property, value in kwargs.items():
+            # depending on whether value is an iterable or not, we must
+            # unpack it's value (when **kwargs is request.form, some values
+            # will be a 1-element list)
+            if hasattr(value, '__iter__') and not isinstance(value, str):
+                # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
+                value = value[0]
+
+            setattr(self, property, value)
+
+    def __repr__(self):
+        return str(self.Name)
+    
