b88b564d55d336ebf6f5ec298e36e90d6240b665
Felice Moretta <felice_moretta@hotmail.it>
2023-10-04 02:54:43 +0200

    Rinominata classe Aggiunto metodo per la verifica dei file ammessi

--- apps/api/utils.py
+++ apps/api/utils.py
@@ -2,9 +2,7 @@ from apps import db
 from apps.databases.models import Capec
 from flask import current_app as app
 
-class AttackPatternUtils:
-    def __init__(self):
-        pass
+class AttackPatternAPIUtils:
 
     def get_child_attack_patterns_by_id(self, parent_id):
         try:
@@ -22,10 +20,15 @@ class AttackPatternUtils:
                 childs = childs + self.get_child_attack_patterns_recursive(child)
             return childs
 
-    def get_child_attack_patterns(self, parent_ids, show_tree=False, get_df=False):
+    def get_child_attack_patterns(self, parent_ids, show_tree=False):
         if type(parent_ids) is not list: parent_ids = [parent_ids]
         childs = [parent_id for parent_id in parent_ids]
         if show_tree:
             childs = childs + [child for parent_id in parent_ids for child in self.get_child_attack_patterns_recursive(parent_id)]
         childs = list(set(childs))
         return childs # return only the ids, not the dataframe
+    
+class APIUtils:
+
+    def allowed_file(self, filename, allowed_extensions):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
