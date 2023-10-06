7d3b261f7f4cca83f496c11fca3963a415aa38ba
Felice Moretta <felice_moretta@hotmail.it>
2023-10-06 23:09:49 +0200

    Aggiunta sezione con tutti i comandi, con possibilità di scaricare il file .sh o copiarlo negli appunti.

--- apps/static/assets/js/pages/macm-detail.js
+++ apps/static/assets/js/pages/macm-detail.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+    getAllCommands();
+    $('.command-input').on('change', function() {
+        getAllCommands();
+    });
+
     $('#ExpandToggle').prop('checked', false);
     $('#ExpandToggle').on('click', function() {
         if ($(this).hasClass('active')) {
@@ -10,23 +15,60 @@ $(document).ready(function() {
     $("#accordionOne [aria-expanded='false']").each(function() {
         $(this).closest('.accordion-item').children('.collapse').collapse('hide');
     });
-    $('.command').children('button').click(function() {
-        var command = '';
-        $(this).parent('.command').find('.command-input').each(function(el) {
+    $('.command').children('button').click(copySingleCommand);
+    $('#copyAllCommands').click(copyAllCommands);
+});
+
+function copySingleCommand(element) {
+    let command = '';
+    $(this).parent('.command').find('.command-input').each(function(el) {
+        if(this.nodeName == 'SPAN') {
+            command += $(this).text();
+        }
+        else if(this.nodeName == 'INPUT') {
+            let value = $(this).val();
+            if(value == '') {
+                value = $(this).attr('placeholder');
+            }
+            command += value;
+        }
+    });
+    navigator.clipboard.writeText(command);
+}
+
+function getAllCommands(){
+    let commands = [];
+    $('.command').each(function() {
+        let command = '';
+        $(this).find('.command-input').each(function(el) {
             if(this.nodeName == 'SPAN') {
                 command += $(this).text();
             }
             else if(this.nodeName == 'INPUT') {
-                var value = $(this).val();
+                let value = $(this).val();
                 if(value == '') {
                     value = $(this).attr('placeholder');
                 }
                 command += value;
             }
         });
-        navigator.clipboard.writeText(command);
+        commands.push(command);
     });
-});
+    commands = Array.from(new Set(commands));
+    $('#allCommands').text(commands.join('\n'));
+    window.Prism.highlightAll();
+}
 
+function copyAllCommands() {
+    navigator.clipboard.writeText($('#allCommands').text());
+}
 
-
+function downloadAllCommands(filename){
+    const link = document.createElement("a");
+    let blob = new Blob([$('#allCommands').text()], {type: "text/plain;charset=utf-8"});
+    link.href = URL.createObjectURL(blob);
+    link.download = filename || "commands.sh";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
