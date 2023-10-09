0adf9acf145584af1c114e833a8a35473f246533
Felice Moretta <felice_moretta@hotmail.it>
2023-10-09 16:57:46 +0200

    Ripristinato funzionamento del search pane

--- apps/static/assets/js/pages/capec.js
+++ apps/static/assets/js/pages/capec.js
@@ -28,24 +28,24 @@ $(window).on('load', function() {
         fixedColumns: {
             left: 1
         },
-        // columnDefs: [
-        //     {
-        //         targets: 0,
-        //         className: 'noVis'
-        //     },
-        //     {
-        //         targets: [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 17, 19, 20, 21, 22, 23, 25, 26, 27],
-        //         searchPanes: {
-        //             show: false,
-        //         },
-        //     },
-        //     {
-        //         targets: [9, 14, 18, 24],
-        //         searchPanes: {
-        //             show: true,
-        //         },
-        //     },
-        // ],
+        columnDefs: [
+            {
+                targets: 0,
+                className: 'noVis'
+            },
+            {
+                targets: [0, 1, 2, 3, 4, 5, 6, 7, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23],
+                searchPanes: {
+                    show: false,
+                },
+            },
+            {
+                targets: [8, 11, 20],
+                searchPanes: {
+                    show: true,
+                },
+            },
+        ],
         lengthMenu: [
             [ 10, 25, 50, -1 ],
             [ '10 rows', '25 rows', '50 rows', 'Show all' ]
