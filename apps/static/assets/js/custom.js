7b1afb89620aac64b989309d52788fe14703b0cc
Felice Moretta <felice_moretta@hotmail.it>
2023-10-11 11:32:15 +0200

    Introdotto modal in base.html

--- apps/static/assets/js/custom.js
+++ apps/static/assets/js/custom.js
@@ -1,1 +1,15 @@
 codeInput.registerTemplate("syntax-highlighted", codeInput.templates.prism(Prism, []));
+
+function showModal(title, response){
+    $("#modal-title").text(title);
+    let messages = "";
+    if (typeof response === "string") {
+        messages = response;
+    } else {
+        for (let key in response) {
+            messages += response[key] + "\n";
+        }
+    }
+    $("#modal-body-text").text(messages);
+    $("#modal-upload").modal("show");
+}
