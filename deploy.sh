fb1537d5b5cb8e2411d50f76930639616b697050
Felice Moretta <felice_moretta@hotmail.it>
2023-11-01 15:22:22 +0100

    modifiche parametri deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-/usr/local/bin/gunicorn --pythonpath ~/WebTesiBs5/ -c ~/WebTesiBs5/gunicorn-cfg.py run:app
+/home/webapp/.pyenv/versions/3.11.6/envs/WebApp/bin/gunicorn --pythonpath ~/WebTesiBs5/ -c ~/WebTesiBs5/gunicorn-cfg.py run:app
--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -3,9 +3,11 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
-bind = webapp.sock
+bind = "unix:/home/webapp/sockets/webapp.sock"
 workers = 1
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
+user = 'webapp'
+group = 'webapp'
