7a79bff24ad7fd9202e99a711dd226aebc423c0f
fefox97 <felice.moretta@studenti.unicampania.it>
2025-01-14 22:31:34 +0100

    ripristinata configurazione gunicorn

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -4,10 +4,9 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 # bind = "unix:/home/webapp/sockets/webapp.sock"
-workers = 5
+workers = 1
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
 bind = '0.0.0.0:5000'
-timeout = 3000
