81e99f01479e88ecd1a392f8b553cec3125ecdc5
fefox97 <felice.moretta@studenti.unicampania.it>
2025-01-18 14:59:12 +0100

    fix password nullable

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -19,7 +19,7 @@ class Users(db.Model, UserMixin):
     id            = db.Column(db.Integer, primary_key=True)
     username      = db.Column(db.String(64), unique=True)
     email         = db.Column(db.String(64), unique=True)
-    password      = db.Column(db.String(255), nullable=False)
+    password      = db.Column(db.String(255))
     active        = db.Column(db.Boolean, default=True, nullable=False)
     created_on    = db.Column(db.DateTime, default=db.func.now())
     updated_on    = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
