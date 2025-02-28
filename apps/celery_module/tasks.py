006d4b0bd5e2b8b4ce5b8bc2c049f0781d68e707
fefox97 <felice_moretta@hotmail.it>
2025-02-28 23:21:34 +0100

    Retry automatico della generazione del report se l'LLM dà risposta vuota. Spostata inizializzazione del database sql in create_app

--- apps/celery_module/tasks.py
+++ apps/celery_module/tasks.py
@@ -1,3 +1,4 @@
+import time
 from flask import current_app as app
 import requests
 import json
@@ -5,32 +6,38 @@ from apps import celery
 from apps.databases.models import Settings
 
 @celery.task
-def query_llm(prompt):
+def query_llm(prompt, max_tries=10, sleep_time=1):
     with app.app_context():
         try:
-            response = requests.post(
-                url="https://openrouter.ai/api/v1/chat/completions",
-                headers={
-                    "Authorization": f"Bearer {app.config["OPENROUTER_API_KEY"]}",
-                    "HTTP-Referer": app.config["URL"],
-                    "X-Title": app.config["SITE_NAME"],
-                },
-                data=json.dumps({
-                    "model": Settings.query.filter_by(key='pentest_report_ai_model').first().value,
-                    "messages": [
-                    {
-                        "role": "user",
-                        "content": prompt
-                    }
-                    ]
-                }),
-                timeout=100
-            )
-            # check if the response has choices
-            if 'error' in response.json():
-                raise Exception(response.json()['error'])
-            response_text = response.json()['choices'][0]['message']['content']
-            return response_text
+            for i in range(max_tries):
+                response = requests.post(
+                    url="https://openrouter.ai/api/v1/chat/completions",
+                    headers={
+                        "Authorization": f"Bearer {app.config["OPENROUTER_API_KEY"]}",
+                        "HTTP-Referer": app.config["URL"],
+                        "X-Title": app.config["SITE_NAME"],
+                    },
+                    data=json.dumps({
+                        "model": Settings.query.filter_by(key='pentest_report_ai_model').first().value,
+                        "messages": [
+                        {
+                            "role": "user",
+                            "content": prompt
+                        }
+                        ]
+                    }),
+                    timeout=100
+                )
+                # check if the response has choices
+                if 'error' in response.json():
+                    raise Exception(response.json()['error'])
+                response_text = response.json()['choices'][0]['message']['content']
+                if response_text == "":
+                    app.logger.info(f"Empty response from LLM, retrying in {sleep_time} seconds")
+                    time.sleep(sleep_time)
+                else:
+                    return response_text
+            raise Exception("Maximum tries reached, no response from LLM")
         
         except:
             raise Exception("Error making the request to LLM")
