31dfb183c9519d8113434be1028b0b91d47d054e
fefox97 <felice_moretta@hotmail.it>
2025-11-29 02:08:56 +0100

    feat(macm-editor): Implement MACM Editor with diagram and palette functionality
    - Added macm-editor.js for managing the MACM diagram and its interactions.
    - Introduced a context menu for node and link operations (cut, copy, paste, delete, undo, redo).
    - Created a responsive layout for the MACM Editor with a diagram area and asset type palette.
    - Implemented a modal for generating and displaying Cypher queries from the diagram.
    - Added styles for the MACM Editor components in _custom.scss.
    - Updated scripts.html to include GoJS and related extensions.
    - Enhanced macm-manager.html to integrate the MACM Editor and its functionalities.
    - Created modals for Canvas to MACM conversion and MACM Editor information.

--- apps/macm/routes.py
+++ apps/macm/routes.py
@@ -25,12 +25,13 @@ def macm():
         usersPerApp = MacmUser.usersPerApp()
         owners = MacmUser.ownerPerApp()
         macms = MacmUser.query.join(App).filter(MacmUser.UserID==current_user.id).with_entities(App.AppID, App.Name.label('AppName'), App.Created_at.label('CreatedAt'), MacmUser.IsOwner).all()
+        asset_types = AssetTypes.get_all_asset_types()
         if len(macms) == 0:
             macms = None
     except Exception as error:
         macms = None
         raise error
-    return render_template('macm/macm-manager.html', segment=get_segment(request), macms=macms, users=users, usersPerApp=usersPerApp, owners=owners, users_dict=users_dict, upload_macm_form=upload_macm_form, upload_docker_compose_form=upload_docker_compose_form)
+    return render_template('macm/macm-manager.html', segment=get_segment(request), macms=macms, users=users, usersPerApp=usersPerApp, owners=owners, users_dict=users_dict, upload_macm_form=upload_macm_form, upload_docker_compose_form=upload_docker_compose_form, asset_types=asset_types)
 
 @blueprint.route('/macm-viewer', methods=['GET'])
 @auth_required()
